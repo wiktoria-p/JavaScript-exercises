511cb9bfd9c3a1b4fa2a89fcedab42feeb940e7f
Wiktoria Pietrzyk <83282608+wiktoria-p@users.noreply.github.com>
2022-12-17 10:02:48 +0100

    Add files via upload

---
+++ script6.js
@@ -0,0 +1,45 @@
+// let celsius
+// let temp 
+// const fahrenheit = (c) => {
+//     celsius = c;
+//     temp = celsius * 1.8 + 32
+
+//     console.log(`${celsius} C = ${temp} F`);
+// }
+
+// fahrenheit(30)
+
+// const num = 10
+// let numbers = []
+
+// for (i=0; i<num; i++) {
+//     numbers.push(i)
+// }
+
+// console.log(numbers);
+
+// const checkNumbers = number => {
+//     if (number % 3 === 0 && number !== 0) {
+//        console.log(`x jest podzielna przez 3`); 
+//     } 
+//     else{
+//         console.log(`x nie jest podzielna przez 3`);
+//     }
+// }
+// numbers.forEach(checkNumbers);
+// console.log(`udało się`);
+
+// let score
+// function add(x, y) {
+//     let score = x + y ;
+//     if  (score % 2 == 0){
+//         console.log(`liczba ${score} jest parzysta`);
+//     }
+//     else {
+//         console.log(`liczba ${score} jest nieparzysta`);
+//     }
+    
+// }
+
+// add(354, 8773)
+
